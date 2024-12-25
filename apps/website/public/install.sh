2ce735bae099237a0ba397d416a5f167e9b0177e
Khiet Tam Nguyen <tamkhietnguyen1@gmail.com>
2024-12-25 16:35:52 +1100

    refactor: install script allows for custom ports

--- apps/website/public/install.sh
+++ apps/website/public/install.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
+
 install_dokploy() {
+    HTTP_PORT=${HTTP_PORT:-80}
+    HTTPS_PORT=${HTTPS_PORT:-443}
+    APP_PORT=${APP_PORT:-3000}
+
     if [ "$(id -u)" != "0" ]; then
         echo "This script must be run as root" >&2
         exit 1
@@ -17,15 +22,20 @@ install_dokploy() {
         exit 1
     fi
 
-    # check if something is running on port 80
-    if ss -tulnp | grep ':80 ' >/dev/null; then
-        echo "Error: something is already running on port 80" >&2
+    # check if something is running on HTTP_PORT (default 80)
+    if ss -tulnp | grep ":$HTTP_PORT " >/dev/null; then
+        echo "Error: something is already running on port $HTTP_PORT" >&2
         exit 1
     fi
 
-    # check if something is running on port 443
-    if ss -tulnp | grep ':443 ' >/dev/null; then
-        echo "Error: something is already running on port 443" >&2
+    # check if something is running on HTTPS_PORT (default 443)
+    if ss -tulnp | grep ":$HTTPS_PORT " >/dev/null; then
+        echo "Error: something is already running on port $HTTPS_PORT" >&2
+        exit 1
+    fi
+
+    if ss -tulnp | grep ":$APP_PORT " >/dev/null; then
+        echo "Error: something is already running on port $APP_PORT" >&2
         exit 1
     fi
 
@@ -86,11 +96,15 @@ install_dokploy() {
       --mount type=bind,source=/var/run/docker.sock,target=/var/run/docker.sock \
       --mount type=bind,source=/etc/dokploy,target=/etc/dokploy \
       --mount type=volume,source=dokploy-docker-config,target=/root/.docker \
-      --publish published=3000,target=3000,mode=host \
+      --publish published=$HTTP_PORT,target=80,mode=host \
+      --publish published=$HTTPS_PORT,target=443,mode=host \
+      --publish published=$APP_PORT,target=3000,mode=host \
       --update-parallelism 1 \
       --update-order stop-first \
       --constraint 'node.role == manager' \
       -e ADVERTISE_ADDR=$advertise_addr \
+      -e TRAEFIK_SSL_PORT=$HTTPS_PORT \
+      -e TRAEFIK_PORT=$HTTP_PORT \
       dokploy/dokploy:latest
 
     GREEN="\033[0;32m"
@@ -113,7 +127,7 @@ install_dokploy() {
     echo ""
     printf "${GREEN}Congratulations, Dokploy is installed!${NC}\n"
     printf "${BLUE}Wait 15 seconds for the server to start${NC}\n"
-    printf "${YELLOW}Please go to http://${formatted_addr}:3000${NC}\n\n"
+    printf "${YELLOW}Please go to http://${formatted_addr}:${APP_PORT}${NC}\n\n"
 }
 
 update_dokploy() {
