321bf73b83091a203a8ca3a320355931683b1deb
Mauricio Siu <siumauricio@icloud.com>
2026-08-14 11:00:41 -0600

    Remove @swc/helpers dependency and update outputFileTracingIncludes in next.config.mjs

--- apps/docs/next.config.mjs
+++ apps/docs/next.config.mjs
@@ -6,6 +6,9 @@ const withMDX = createMDX();
 const config = {
 	reactStrictMode: true,
 	output: "standalone",
+	outputFileTracingIncludes: {
+		"/**": ["./node_modules/.pnpm/@swc+helpers@*/node_modules/@swc/helpers/**"],
+	},
 	images: {
 		remotePatterns: [
 			{
